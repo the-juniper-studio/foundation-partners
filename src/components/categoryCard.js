9916fd849c5eefca68c790df248e552150f12e5f
Nicki Lever <nicki@thejuniperstudio.com>
2024-10-07 17:27:16 +0100

    cateogry card authros

--- src/components/categoryCard.js
+++ src/components/categoryCard.js
@@ -4,13 +4,13 @@ import { GatsbyImage } from 'gatsby-plugin-image'
 
 const CategoryCard = ({ item }) => {
   const itemData = item.node.data
-  // const authorData = itemData.author?.document.data
-  // const publishDate = item.node.first_publication_date_formatted
-  // let timeToRead
+  const authorData = itemData.author?.document.data
+  const publishDate = item.node.first_publication_date_formatted
+  let timeToRead
 
-  // const wordsPerMinute = 200
-  // let wordCount = itemData.page_text?.text.split(' ').length
-  // timeToRead = Math.ceil(wordCount / wordsPerMinute)
+  const wordsPerMinute = 200
+  let wordCount = itemData.page_text?.text.split(' ').length
+  timeToRead = Math.ceil(wordCount / wordsPerMinute)
 
   return (
     <li className='component-card ease relative flex transform flex-col overflow-hidden border-b-4 border-brandMustard group'>
@@ -21,13 +21,25 @@ const CategoryCard = ({ item }) => {
           </div>
         )}
         <div className='flex flex-col -mt-20 bg-brandCream dark:bg-brandBlack z-10 m-3 p-6 h-full'>
-          <div className='mb-6 space-y-4 prose lg:prose-xl dark:prose-invert'>
+          <div className='mb-6 space-y-4 prose lg:prose-xl dark:prose-invert h-full'>
+            {authorData && (
+              <div className='mt-auto flex flex-row mb-6 not-prose items-center'>
+                <div className='relative mr-3 flex-shrink-0'>
+                  <GatsbyImage className='rounded-full h-10 w-10 bg-brandMustard' image={authorData.image.gatsbyImageData} alt={authorData.image.alt || ''} />
+                </div>
+                <div>
+                  <p className='font-medium text-base'>{authorData.name}</p>
+                  <div className='flex text-sm text-neutral-500 dark:text-neutral-300'>{timeToRead} min read</div>
+                </div>
+              </div>
+            )}
             <h3 className='group-hover:text-brandRust'>{itemData.page_title.text}</h3>
             <div className='my-3'>
               {itemData.page_text.text.split(' ').slice(0, 20).join(' ')}
               &hellip;
             </div>
           </div>
+
           <p className='mb-3 mt-auto text-lg lg:text-xl font-semibold underline group-hover:no-underline'>Read more</p>
         </div>
       </PrismicLink>
